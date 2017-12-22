3a11c771ce413881f708a66fbd2afca5e53f469e
Dylan Powers <dylandpowers@gmail.com>
2017-12-21 21:24:32 -0700

    included more complex derivatives

--- index.js
+++ index.js
@@ -32,9 +32,17 @@ const START_PROBLEM = "Alright, here we go. ";
 
 const REPEAT_WELCOME = "Come on! You have to get out of bed at some point!";
 
-const RIGHT_ANSWER = "Great job you mother <say-as interpret-as='expletive'>fucker</say-as>! You're well on the way to a wonderful day. ";
+const RIGHT_ANSWER = "Great job you star! You're well on the way to a wonderful day. ";
 
 const WRONG_ANSWER = "Sorry, that's not quite right. Please try again. ";
+
+const data = [
+	{func: "sine", derivative: "cosine"},
+	{func: "cosine", derivative: "negative sine"},
+	{func: "natural logarithm", derivative: "one divided by"},
+	// If we select this one, we will just generate a random coefficient and exponent
+	{func: "x", derivative: "foo"}
+];
 
 // Generic handlers
 const handlers = {
@@ -101,14 +109,31 @@ const problemHandlers = Alexa.CreateStateHandler(states.PROBLEM, {
 	"Problem": function() 
 	{
 		this.attributes["response"] = START_PROBLEM;
-		//this.attributes["exponent"] = 0;
-		this.emitWithState("AskQuestion");
+		
+		let index = getRandomInt(0, data.length-1);
+		let item = data[index];
+		var property = item["func"];
+		this.attributes["func"] = property;
+		if (property === "x") { // If we need an exponent and a coefficient generated
+			this.emitWithState("AskEasyQuestion");
+		} else {  
+
+			// This is the case where we need a more complex derivative
+			this.attributes["derivative"] = item["derivative"];
+			this.emitWithState("AskHardQuestion"); 
+		}
 	},
-	"AskQuestion" : function() 
+	"AskEasyQuestion" : function() 
 	{
-		this.attributes["exponent"] = getRandomIntStr(1, 10);
-		this.attributes["coefficient"] = getRandomIntStr(1, 10);
+		this.attributes["exponent"] = getRandomInt(1, 10).toString();
+		this.attributes["coefficient"] = getRandomInt(1, 10).toString();
 		let question = "What is the derivative of " + this.attributes["coefficient"] + " <say-as interpret-as='characters'>x</say-as> to the power of " + this.attributes["exponent"] + "?";
+		let speech = this.attributes["response"] + question;
+		this.emit(":ask", speech, question);
+	},
+	"AskHardQuestion" : function() 
+	{
+		let question = "What is the derivative of " + this.attributes["func"] + " of <say-as interpret-as='character'>x</say-as>?";
 		let speech = this.attributes["response"] + question;
 		this.emit(":ask", speech, question);
 	},
@@ -116,19 +141,38 @@ const problemHandlers = Alexa.CreateStateHandler(states.PROBLEM, {
 	{
 		this.attributes["response"] = "";
 
+		// Case where we are dealing with a simple function
+		if (this.attributes["func"] === "x") {
 		// Get the final answer (coefficient and exponent) from the user
-		let answerCoeff = this.event.request.intent.slots.coeff.value;
-		let answerExp = this.event.request.intent.slots.exp.value;
+			let answerCoeff = this.event.request.intent.slots.coeff.value;
+			let answerExp = this.event.request.intent.slots.exp.value;
 
-		// What Alexa should say back
-		if (answerCoeff == (this.attributes["exponent"]*this.attributes["coefficient"]) && answerExp == (this.attributes["exponent"] - 1)) {
-			this.attributes["response"] = RIGHT_ANSWER;
-			this.response.speak(this.attributes["response"]);
-			this.emit(":responseReady");
+			// What Alexa should say back based upon what the answer is
+			if (answerCoeff == (this.attributes["exponent"]*this.attributes["coefficient"]) && answerExp == (this.attributes["exponent"] - 1)) {
+				this.attributes["response"] = RIGHT_ANSWER;
+				this.response.speak(this.attributes["response"]);
+				this.emit(":responseReady");
+			} else {
+				this.attributes["response"] = WRONG_ANSWER;
+				this.response.speak(this.attributes["response"]);
+				this.emitWithState("AskEasyQuestion");
+			}
 		} else {
-			this.attributes["response"] = WRONG_ANSWER;
-			//this.response.speak(this.attributes["response"]);
-			this.emitWithState("AskQuestion");
+			// This is the case where we are dealing with more complex derivatives
+			// Remember that we have stored this.attributes["derivative"]
+			let answerFunc = this.event.request.intent.slots.function.value;
+
+			// What Alexa should say back based upon what the answer is
+			if (answerFunc == this.attributes["derivative"]) {
+				this.attributes["response"] = RIGHT_ANSWER;
+				this.response.speak(this.attributes["response"]);
+				this.emit(":responseReady");
+			} else {
+				this.attributes["response"] = WRONG_ANSWER;
+				this.response.speak(this.attributes["response"]);
+				this.emitWithState("AskEasyQuestion");
+			}
+
 		}
 	},
 	"AMAZON.StopIntent" : function()
@@ -148,12 +192,6 @@ const problemHandlers = Alexa.CreateStateHandler(states.PROBLEM, {
 });
 
 // Random integer calculation
-function getRandomIntStr(min, max) {
-	return (Math.floor(Math.random() * (max - min + 1)) + min).toString();
+function getRandomInt(min, max) {
+	return (Math.floor(Math.random() * (max - min + 1)) + min);
 }
-
-// Calculate the derivative, but only of a single term
-// Parameter 'a' is power of x
-
-
-
